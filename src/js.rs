672863b17ffc991602b8ff3fe50d4a13d93d916e
David Craven <david@craven.ch>
2021-12-15 10:31:24 +0100

    Fix build.

--- src/js.rs
+++ src/js.rs
@@ -230,11 +230,11 @@ impl TsGenerator {
     }
 
     fn type_ident(&self, s: &str) -> String {
-        sanitize_identifier(&s.to_camel_case())
+        sanitize_identifier(&s.to_upper_camel_case())
     }
 
     fn ident(&self, s: &str) -> String {
-        sanitize_identifier(&s.to_mixed_case())
+        sanitize_identifier(&s.to_lower_camel_case())
     }
 }
 
@@ -695,11 +695,11 @@ impl JsGenerator {
     }
 
     fn type_ident(&self, s: &str) -> String {
-        sanitize_identifier(&s.to_camel_case())
+        sanitize_identifier(&s.to_upper_camel_case())
     }
 
     fn ident(&self, s: &str) -> String {
-        sanitize_identifier(&s.to_mixed_case())
+        sanitize_identifier(&s.to_lower_camel_case())
     }
 }
 
